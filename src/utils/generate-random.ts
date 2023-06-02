fdd2b1fbc5bdb03b3744445df8d7622a5c1c31ea
abj1389 <abj_13@hotmail.com>
2023-06-02 17:52:00 +0200

    cleaned project

--- src/utils/generate-random.ts
+++
@@ -1,8 +0,0 @@
-export const generateRandom = (min = 0, max = 100): number => {
-  const difference = max - min;
-  let rand = Math.random();
-  rand = Math.floor(rand * difference);
-  rand = rand + min;
-
-  return rand;
-};
